67dd5b304328831107244ef5555b140d6fad9ca0
Matthew Ballance <matt.ballance@gmail.com>
2026-02-11 04:13:21 +0000

    Checkpoint
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/V3Width.cpp
+++ src/V3Width.cpp
@@ -4511,8 +4511,7 @@ class WidthVisitor final : public VNVisitor {
                 }
                 // Process arguments - same as class code at line 4554
                 if (withp) nodep->addPinsp(withp);
-                // Set didWidth before processing args to avoid recursion
-                nodep->didWidth(true);
+                // Call processFTaskRefArgs which will set didWidth
                 processFTaskRefArgs(nodep);
                 return;
             }
