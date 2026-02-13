8afebc8f732c641ff067ba1efe9a071e9469b271
github action <action@example.com>
2026-02-13 23:23:22 +0000

    Apply 'make format'

--- test_regress/t/t_covergroup_empty.cpp
+++ test_regress/t/t_covergroup_empty.cpp
@@ -1,23 +1,24 @@
 // -*- mode: C++; c-file-style: "cc-mode" -*-
 // Simple test harness for t_covergroup_empty - provides clock
 
-#include "Vt_covergroup_empty.h"
 #include "verilated.h"
+
+#include "Vt_covergroup_empty.h"
 
 int main(int argc, char** argv) {
     Verilated::commandArgs(argc, argv);
     Vt_covergroup_empty* top = new Vt_covergroup_empty;
-    
+
     // Run for 20 cycles
     for (int i = 0; i < 20; i++) {
         top->clk = 0;
         top->eval();
         top->clk = 1;
         top->eval();
-        
+
         if (Verilated::gotFinish()) break;
     }
-    
+
     delete top;
     return 0;
 }
