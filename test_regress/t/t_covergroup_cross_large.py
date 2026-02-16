bb74bfd36018f63bb67de03599b4ba2011d741c8
github action <action@example.com>
2026-02-16 18:45:35 +0000

    Apply 'make format'

--- test_regress/t/t_covergroup_cross_large.py
+++ test_regress/t/t_covergroup_cross_large.py
@@ -10,10 +10,8 @@
 import vltest_bootstrap
 
 test.scenarios('simulator')
-test.compile(
-    make_main=False,
-    verilator_flags2=["--coverage-user", "--exe", "t/t_covergroup_cross_large_main.cpp"]
-)
+test.compile(make_main=False,
+             verilator_flags2=["--coverage-user", "--exe", "t/t_covergroup_cross_large_main.cpp"])
 test.execute(check_finished=True)
 
 test.passes()
