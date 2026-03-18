35efc3c552c9793f3d3d58d210edd344ecd0fd85
github action <action@example.com>
2026-03-18 01:45:25 +0000

    Apply 'make format'

--- src/V3AstNodes.cpp
+++ src/V3AstNodes.cpp
@@ -3699,9 +3699,7 @@ void AstWith::dumpJson(std::ostream& str) const {
 
 void AstCovergroup::dump(std::ostream& str) const { this->AstNode::dump(str); }
 
-void AstCovergroup::dumpJson(std::ostream& str) const {
-    dumpJsonGen(str);
-}
+void AstCovergroup::dumpJson(std::ostream& str) const { dumpJsonGen(str); }
 
 void AstCoverpoint::dump(std::ostream& str) const { this->AstNodeFuncCovItem::dump(str); }
 
@@ -3729,9 +3727,7 @@ void AstCoverTransItem::dumpJson(std::ostream& str) const {
     if (m_repType != VTransRepType::NONE) { str << ", \"repType\": " << m_repType.asciiJson(); }
 }
 
-void AstCoverTransSet::dump(std::ostream& str) const {
-    this->AstNode::dump(str);
-}
+void AstCoverTransSet::dump(std::ostream& str) const { this->AstNode::dump(str); }
 
 void AstCoverTransSet::dumpJson(std::ostream& str) const { this->AstNode::dumpJson(str); }
 
