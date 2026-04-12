4ca3ff5ad3381cd64e4ae14ca07328d28fcceb98
github action <action@example.com>
2026-04-12 15:04:13 +0000

    Apply 'make format'

--- src/V3AstNodes.cpp
+++ src/V3AstNodes.cpp
@@ -3563,7 +3563,9 @@ void AstCoverTransItem::dump(std::ostream& str) const {
 
 void AstCoverTransItem::dumpJson(std::ostream& str) const {
     this->AstNode::dumpJson(str);
-    if (m_repType != VTransRepType::NONE) { str << ", \"repType\": " << m_repType.asciiJson(); }  // LCOV_EXCL_BR_LINE
+    if (m_repType != VTransRepType::NONE) {
+        str << ", \"repType\": " << m_repType.asciiJson();
+    }  // LCOV_EXCL_BR_LINE
 }
 
 void AstCoverTransSet::dump(std::ostream& str) const { this->AstNode::dump(str); }
